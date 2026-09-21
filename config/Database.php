c0c03259864254b7305a92f9e0c23dc663b026aa
Erick nicolas <nicolasaguirre0624@gmail.com>
2026-09-21 12:36:55 -0500

    Primer commit aroma

--- config/Database.php
+++ config/Database.php
@@ -1,7 +1,8 @@
 <?php
+
 class Database {
     private $host = "localhost";
-    private $db_name = "tienda_aromas";
+    private $db_name = "proyecto";
     private $username = "root";
     private $password = "";
     public $conn;
@@ -9,12 +10,17 @@ class Database {
     public function conectar() {
         $this->conn = null;
         try {
-            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
-            $this->conn->set_charset("utf8");
-        } catch (Exception $e) {
-            echo "Error de conexión: " . $e->getMessage();
+            $this->conn = new PDO(
+                "mysql:host={$this->host};dbname={$this->db_name};charset=utf8", 
+                $this->username, 
+                $this->password
+            );
+           
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Error de conexión: " . $exception->getMessage();
         }
+        
         return $this->conn;
     }
 }
-?>
---
+++ test.php
@@ -0,0 +1,3 @@
+<?php
+echo "¡Hola Nicolas! PHP está funcionando correctamente.";
+?>
